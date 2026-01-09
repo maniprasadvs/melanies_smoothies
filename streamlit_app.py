08e0ce2b27aedcb97c4946595a579ba2aa176c57
maniprasadvs <maniprasad_vs@yahoo.com>
2026-01-09 13:18:36 -0500

    Update streamlit_app.py
    api request v1.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,9 +36,11 @@ if ingredients_list:
 
     time_to_insert = st.button("Submit")
 
-  smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-  st.text(smoothiefroot_response)
+  
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+ name_on_order +' !')
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+  st.text(smoothiefroot_response.json())
